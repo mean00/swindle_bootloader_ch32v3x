9d6cf58a628e8ea14f7f1db50205d070a3d61873
mean <fixounet@free.fr>
2024-11-12 07:00:40 +0100

    update to 0.17.0 without precommit

--- tinyUsb/src/src/src/osal/osal.h
+++ tinyUsb/src/src/src/osal/osal.h
@@ -28,75 +28,72 @@
 #define _TUSB_OSAL_H_
 
 #ifdef __cplusplus
-extern "C"
-{
+ extern "C" {
 #endif
 
 #include "common/tusb_common.h"
 
-    typedef void (*osal_task_func_t)(void *);
+typedef void (*osal_task_func_t)( void * );
 
 // Timeout
-#define OSAL_TIMEOUT_NOTIMEOUT (0)             // Return immediately
-#define OSAL_TIMEOUT_NORMAL (10)               // Default timeout
-#define OSAL_TIMEOUT_WAIT_FOREVER (UINT32_MAX) // Wait forever
-#define OSAL_TIMEOUT_CONTROL_XFER OSAL_TIMEOUT_WAIT_FOREVER
+#define OSAL_TIMEOUT_NOTIMEOUT     (0)          // Return immediately
+#define OSAL_TIMEOUT_NORMAL        (10)         // Default timeout
+#define OSAL_TIMEOUT_WAIT_FOREVER  (UINT32_MAX) // Wait forever
+#define OSAL_TIMEOUT_CONTROL_XFER  OSAL_TIMEOUT_WAIT_FOREVER
 
 // Mutex is required when using a preempted RTOS or MCU has multiple cores
 #if (CFG_TUSB_OS == OPT_OS_NONE) && !TUP_MCU_MULTIPLE_CORE
-#define OSAL_MUTEX_REQUIRED 0
-#define OSAL_MUTEX_DEF(_name)                                                                                          \
-    uint8_t:                                                                                                           \
-    0
+  #define OSAL_MUTEX_REQUIRED   0
+  #define OSAL_MUTEX_DEF(_name) uint8_t :0
 #else
-#define OSAL_MUTEX_REQUIRED 1
-#define OSAL_MUTEX_DEF(_name) osal_mutex_def_t _name
+  #define OSAL_MUTEX_REQUIRED   1
+  #define OSAL_MUTEX_DEF(_name) osal_mutex_def_t _name
 #endif
 
 // OS thin implementation
 #if CFG_TUSB_OS == OPT_OS_NONE
-#include "osal_none.h"
+  #include "osal_none.h"
 #elif CFG_TUSB_OS == OPT_OS_FREERTOS
-#include "osal_freertos.h"
+  #include "osal_freertos.h"
 #elif CFG_TUSB_OS == OPT_OS_MYNEWT
-#include "osal_mynewt.h"
+  #include "osal_mynewt.h"
 #elif CFG_TUSB_OS == OPT_OS_PICO
-#include "osal_pico.h"
+  #include "osal_pico.h"
 #elif CFG_TUSB_OS == OPT_OS_RTTHREAD
-#include "osal_rtthread.h"
+  #include "osal_rtthread.h"
 #elif CFG_TUSB_OS == OPT_OS_RTX4
-#include "osal_rtx4.h"
+  #include "osal_rtx4.h"
 #elif CFG_TUSB_OS == OPT_OS_CUSTOM
-#include "tusb_os_custom.h" // implemented by application
+  #include "tusb_os_custom.h" // implemented by application
 #else
-#error OS is not supported yet
+  #error OS is not supported yet
 #endif
 
-    //--------------------------------------------------------------------+
-    // OSAL Porting API
-    // Should be implemented as static inline function in osal_port.h header
-    /*
-       osal_semaphore_t osal_semaphore_create(osal_semaphore_def_t* semdef);
-       bool osal_semaphore_delete(osal_semaphore_t semd_hdl);
-       bool osal_semaphore_post(osal_semaphore_t sem_hdl, bool in_isr);
-       bool osal_semaphore_wait(osal_semaphore_t sem_hdl, uint32_t msec);
-       void osal_semaphore_reset(osal_semaphore_t sem_hdl); // TODO removed
+//--------------------------------------------------------------------+
+// OSAL Porting API
+// Should be implemented as static inline function in osal_port.h header
+/*
+   osal_semaphore_t osal_semaphore_create(osal_semaphore_def_t* semdef);
+   bool osal_semaphore_delete(osal_semaphore_t semd_hdl);
+   bool osal_semaphore_post(osal_semaphore_t sem_hdl, bool in_isr);
+   bool osal_semaphore_wait(osal_semaphore_t sem_hdl, uint32_t msec);
+   void osal_semaphore_reset(osal_semaphore_t sem_hdl); // TODO removed
 
-       osal_mutex_t osal_mutex_create(osal_mutex_def_t* mdef);
-       bool osal_mutex_delete(osal_mutex_t mutex_hdl)
-       bool osal_mutex_lock (osal_mutex_t sem_hdl, uint32_t msec);
-       bool osal_mutex_unlock(osal_mutex_t mutex_hdl);
+   osal_mutex_t osal_mutex_create(osal_mutex_def_t* mdef);
+   bool osal_mutex_delete(osal_mutex_t mutex_hdl)
+   bool osal_mutex_lock (osal_mutex_t sem_hdl, uint32_t msec);
+   bool osal_mutex_unlock(osal_mutex_t mutex_hdl);
 
-       osal_queue_t osal_queue_create(osal_queue_def_t* qdef);
-       bool osal_queue_delete(osal_queue_t qhdl);
-       bool osal_queue_receive(osal_queue_t qhdl, void* data, uint32_t msec);
-       bool osal_queue_send(osal_queue_t qhdl, void const * data, bool in_isr);
-       bool osal_queue_empty(osal_queue_t qhdl);
-    */
-    //--------------------------------------------------------------------+
+   osal_queue_t osal_queue_create(osal_queue_def_t* qdef);
+   bool osal_queue_delete(osal_queue_t qhdl);
+   bool osal_queue_receive(osal_queue_t qhdl, void* data, uint32_t msec);
+   bool osal_queue_send(osal_queue_t qhdl, void const * data, bool in_isr);
+   bool osal_queue_empty(osal_queue_t qhdl);
+*/
+//--------------------------------------------------------------------+
 
 #ifdef __cplusplus
-}
+ }
 #endif
 
 #endif /* _TUSB_OSAL_H_ */
--- tinyUsb/src/src/src/portable/wch/ch32_usbfs_reg.h
+++ tinyUsb/src/src/src/portable/wch/ch32_usbfs_reg.h
@@ -36,24 +36,24 @@
 #endif
 
 #if CFG_TUSB_MCU == OPT_MCU_CH32F20X
-#include <ch32f20x.h>
+  #include <ch32f20x.h>
 #elif CFG_TUSB_MCU == OPT_MCU_CH32V103
-#include <ch32v10x.h>
-typedef struct
-{
-    __IO uint8_t BASE_CTRL;
-    __IO uint8_t UDEV_CTRL;
-    __IO uint8_t INT_EN;
-    __IO uint8_t DEV_ADDR;
-    __IO uint8_t Reserve0;
-    __IO uint8_t MIS_ST;
-    __IO uint8_t INT_FG;
-    __IO uint8_t INT_ST;
+  #include <ch32v10x.h>
+  typedef struct
+  {
+    __IO uint8_t  BASE_CTRL;
+    __IO uint8_t  UDEV_CTRL;
+    __IO uint8_t  INT_EN;
+    __IO uint8_t  DEV_ADDR;
+    __IO uint8_t  Reserve0;
+    __IO uint8_t  MIS_ST;
+    __IO uint8_t  INT_FG;
+    __IO uint8_t  INT_ST;
     __IO uint32_t RX_LEN;
-    __IO uint8_t UEP4_1_MOD;
-    __IO uint8_t UEP2_3_MOD;
-    __IO uint8_t UEP5_6_MOD;
-    __IO uint8_t UEP7_MOD;
+    __IO uint8_t  UEP4_1_MOD;
+    __IO uint8_t  UEP2_3_MOD;
+    __IO uint8_t  UEP5_6_MOD;
+    __IO uint8_t  UEP7_MOD;
     __IO uint32_t UEP0_DMA;
     __IO uint32_t UEP1_DMA;
     __IO uint32_t UEP2_DMA;
@@ -63,40 +63,40 @@ typedef struct
     __IO uint32_t UEP6_DMA;
     __IO uint32_t UEP7_DMA;
     __IO uint16_t UEP0_TX_LEN;
-    __IO uint8_t UEP0_TX_CTRL;
-    __IO uint8_t UEP0_RX_CTRL;
+    __IO uint8_t  UEP0_TX_CTRL;
+    __IO uint8_t  UEP0_RX_CTRL;
     __IO uint16_t UEP1_TX_LEN;
-    __IO uint8_t UEP1_TX_CTRL;
-    __IO uint8_t UEP1_RX_CTRL;
+    __IO uint8_t  UEP1_TX_CTRL;
+    __IO uint8_t  UEP1_RX_CTRL;
     __IO uint16_t UEP2_TX_LEN;
-    __IO uint8_t UEP2_TX_CTRL;
-    __IO uint8_t UEP2_RX_CTRL;
+    __IO uint8_t  UEP2_TX_CTRL;
+    __IO uint8_t  UEP2_RX_CTRL;
     __IO uint16_t UEP3_TX_LEN;
-    __IO uint8_t UEP3_TX_CTRL;
-    __IO uint8_t UEP3_RX_CTRL;
+    __IO uint8_t  UEP3_TX_CTRL;
+    __IO uint8_t  UEP3_RX_CTRL;
     __IO uint16_t UEP4_TX_LEN;
-    __IO uint8_t UEP4_TX_CTRL;
-    __IO uint8_t UEP4_RX_CTRL;
+    __IO uint8_t  UEP4_TX_CTRL;
+    __IO uint8_t  UEP4_RX_CTRL;
     __IO uint16_t UEP5_TX_LEN;
-    __IO uint8_t UEP5_TX_CTRL;
-    __IO uint8_t UEP5_RX_CTRL;
+    __IO uint8_t  UEP5_TX_CTRL;
+    __IO uint8_t  UEP5_RX_CTRL;
     __IO uint16_t UEP6_TX_LEN;
-    __IO uint8_t UEP6_TX_CTRL;
-    __IO uint8_t UEP6_RX_CTRL;
+    __IO uint8_t  UEP6_TX_CTRL;
+    __IO uint8_t  UEP6_RX_CTRL;
     __IO uint16_t UEP7_TX_LEN;
-    __IO uint8_t UEP7_TX_CTRL;
-    __IO uint8_t UEP7_RX_CTRL;
+    __IO uint8_t  UEP7_TX_CTRL;
+    __IO uint8_t  UEP7_RX_CTRL;
     __IO uint32_t Reserve1;
     __IO uint32_t OTG_CR;
     __IO uint32_t OTG_SR;
-} USBOTG_FS_TypeDef;
+  } USBOTG_FS_TypeDef;
 
-#define USBOTG_FS ((USBOTG_FS_TypeDef *)0x40023400)
+  #define USBOTG_FS ((USBOTG_FS_TypeDef *) 0x40023400)
 #elif CFG_TUSB_MCU == OPT_MCU_CH32V20X
-#include <ch32v20x.h>
+  #include <ch32v20x.h>
 #elif CFG_TUSB_MCU == OPT_MCU_CH32V307
-#include <ch32v30x.h>
-#define USBHD_IRQn OTG_FS_IRQn
+  #include <ch32v30x.h>
+  #define USBHD_IRQn OTG_FS_IRQn
 #endif
 
 #ifdef __GNUC__
@@ -104,72 +104,72 @@ typedef struct
 #endif
 
 // CTRL
-#define USBFS_CTRL_DMA_EN (1 << 0)
-#define USBFS_CTRL_CLR_ALL (1 << 1)
+#define USBFS_CTRL_DMA_EN    (1 << 0)
+#define USBFS_CTRL_CLR_ALL   (1 << 1)
 #define USBFS_CTRL_RESET_SIE (1 << 2)
-#define USBFS_CTRL_INT_BUSY (1 << 3)
-#define USBFS_CTRL_SYS_CTRL (1 << 4)
-#define USBFS_CTRL_DEV_PUEN (1 << 5)
+#define USBFS_CTRL_INT_BUSY  (1 << 3)
+#define USBFS_CTRL_SYS_CTRL  (1 << 4)
+#define USBFS_CTRL_DEV_PUEN  (1 << 5)
 #define USBFS_CTRL_LOW_SPEED (1 << 6)
 #define USBFS_CTRL_HOST_MODE (1 << 7)
 
 // INT_EN
-#define USBFS_INT_EN_BUS_RST (1 << 0)
-#define USBFS_INT_EN_DETECT (1 << 0)
+#define USBFS_INT_EN_BUS_RST  (1 << 0)
+#define USBFS_INT_EN_DETECT   (1 << 0)
 #define USBFS_INT_EN_TRANSFER (1 << 1)
-#define USBFS_INT_EN_SUSPEND (1 << 2)
-#define USBFS_INT_EN_HST_SOF (1 << 3)
-#define USBFS_INT_EN_FIFO_OV (1 << 4)
-#define USBFS_INT_EN_DEV_NAK (1 << 6)
-#define USBFS_INT_EN_DEV_SOF (1 << 7)
+#define USBFS_INT_EN_SUSPEND  (1 << 2)
+#define USBFS_INT_EN_HST_SOF  (1 << 3)
+#define USBFS_INT_EN_FIFO_OV  (1 << 4)
+#define USBFS_INT_EN_DEV_NAK  (1 << 6)
+#define USBFS_INT_EN_DEV_SOF  (1 << 7)
 
 // INT_FG
-#define USBFS_INT_FG_BUS_RST (1 << 0)
-#define USBFS_INT_FG_DETECT (1 << 0)
+#define USBFS_INT_FG_BUS_RST  (1 << 0)
+#define USBFS_INT_FG_DETECT   (1 << 0)
 #define USBFS_INT_FG_TRANSFER (1 << 1)
-#define USBFS_INT_FG_SUSPEND (1 << 2)
-#define USBFS_INT_FG_HST_SOF (1 << 3)
-#define USBFS_INT_FG_FIFO_OV (1 << 4)
+#define USBFS_INT_FG_SUSPEND  (1 << 2)
+#define USBFS_INT_FG_HST_SOF  (1 << 3)
+#define USBFS_INT_FG_FIFO_OV  (1 << 4)
 #define USBFS_INT_FG_SIE_FREE (1 << 5)
-#define USBFS_INT_FG_TOG_OK (1 << 6)
-#define USBFS_INT_FG_IS_NAK (1 << 7)
+#define USBFS_INT_FG_TOG_OK   (1 << 6)
+#define USBFS_INT_FG_IS_NAK   (1 << 7)
 
 // INT_ST
-#define USBFS_INT_ST_MASK_UIS_ENDP(x) (((x) >> 0) & 0x0F)
+#define USBFS_INT_ST_MASK_UIS_ENDP(x)  (((x) >> 0) & 0x0F)
 #define USBFS_INT_ST_MASK_UIS_TOKEN(x) (((x) >> 4) & 0x03)
 
 // UDEV_CTRL
-#define USBFS_UDEV_CTRL_PORT_EN (1 << 0)
-#define USBFS_UDEV_CTRL_GP_BIT (1 << 1)
+#define USBFS_UDEV_CTRL_PORT_EN   (1 << 0)
+#define USBFS_UDEV_CTRL_GP_BIT    (1 << 1)
 #define USBFS_UDEV_CTRL_LOW_SPEED (1 << 2)
-#define USBFS_UDEV_CTRL_DM_PIN (1 << 4)
-#define USBFS_UDEV_CTRL_DP_PIN (1 << 5)
-#define USBFS_UDEV_CTRL_PD_DIS (1 << 7)
+#define USBFS_UDEV_CTRL_DM_PIN    (1 << 4)
+#define USBFS_UDEV_CTRL_DP_PIN    (1 << 5)
+#define USBFS_UDEV_CTRL_PD_DIS    (1 << 7)
 
 // TX_CTRL
 #define USBFS_EP_T_RES_MASK (3 << 0)
-#define USBFS_EP_T_TOG (1 << 2)
+#define USBFS_EP_T_TOG      (1 << 2)
 #define USBFS_EP_T_AUTO_TOG (1 << 3)
 
-#define USBFS_EP_T_RES_ACK (0 << 0)
-#define USBFS_EP_T_RES_NYET (1 << 0)
-#define USBFS_EP_T_RES_NAK (2 << 0)
+#define USBFS_EP_T_RES_ACK   (0 << 0)
+#define USBFS_EP_T_RES_NYET  (1 << 0)
+#define USBFS_EP_T_RES_NAK   (2 << 0)
 #define USBFS_EP_T_RES_STALL (3 << 0)
 
 // RX_CTRL
 #define USBFS_EP_R_RES_MASK (3 << 0)
-#define USBFS_EP_R_TOG (1 << 2)
+#define USBFS_EP_R_TOG      (1 << 2)
 #define USBFS_EP_R_AUTO_TOG (1 << 3)
 
-#define USBFS_EP_R_RES_ACK (0 << 0)
-#define USBFS_EP_R_RES_NYET (1 << 0)
-#define USBFS_EP_R_RES_NAK (2 << 0)
+#define USBFS_EP_R_RES_ACK   (0 << 0)
+#define USBFS_EP_R_RES_NYET  (1 << 0)
+#define USBFS_EP_R_RES_NAK   (2 << 0)
 #define USBFS_EP_R_RES_STALL (3 << 0)
 
 // token PID
-#define PID_OUT 0
-#define PID_SOF 1
-#define PID_IN 2
+#define PID_OUT   0
+#define PID_SOF   1
+#define PID_IN    2
 #define PID_SETUP 3
 
 #endif // USB_CH32_USBFS_REG_H
